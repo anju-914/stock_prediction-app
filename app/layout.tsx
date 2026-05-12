3cd4acdc9a1e90baf8bcfdc654aaa3104e2b2def
anju-914 <narwalanju108@gmail.com>
2026-05-12 13:02:50 +0530

    implement header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -13,21 +14,23 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Stock Predection",
-  description: "Track real-time stock prices, get personalized alerts and explore deatiled company insights.",
+  title: "Signalist",
+  description: "Track real-time stock prices, get personalized alerts and explore detailed company insights.",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+      <html lang="en" className="dark">
+      <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+      {children}
+      <Toaster />
+      </body>
+      </html>
   );
 }
